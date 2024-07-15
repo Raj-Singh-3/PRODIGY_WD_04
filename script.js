1e34050f528685fea81aa4f43f7b9df42d7643f9
Raj Singh <152046820+Raj-Singh-3@users.noreply.github.com>
2024-07-15 07:13:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const m = document.querySelector("#menu-icon");
+const navbar = document.querySelector(".navbar");
+
+m.onclick = () => {
+  m.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
